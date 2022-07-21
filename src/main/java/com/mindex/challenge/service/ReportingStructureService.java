38f2e09eeeaba7642cb31cf0cb873226170046d5
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 15:35:17 -0500

    Add ReportingStructureService interface

---
+++ src/main/java/com/mindex/challenge/service/ReportingStructureService.java
@@ -0,0 +1,7 @@
+package com.mindex.challenge.service;
+
+import com.mindex.challenge.dto.ReportingStructureDto;
+
+public interface ReportingStructureService {
+    ReportingStructureDto read(String employeeId);
+}
