5783f7fda80c8c7780252ddcad78064d13d46183
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 17:55:11 -0500

    Create initial set of CompensationServiceImpl tests

---
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTests.java
@@ -0,0 +1,135 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.adapter.CompensationAdapter;
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.dto.CompensationDto;
+import com.mindex.challenge.exceptions.CompensationNotFoundException;
+import com.mindex.challenge.exceptions.EmployeeNotFoundException;
+import com.mindex.challenge.service.CompensationService;
+import com.mindex.challenge.utils.CompensationComparer;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompensationServiceImplTests {
+
+    @Autowired
+    CompensationService compensationService;
+
+    @Autowired
+    CompensationAdapter compensationAdapter;
+
+    @MockBean
+    CompensationRepository compensationRepository;
+
+    @MockBean
+    EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("Smoke test for Spring DI")
+    public void contextLoads() {
+        assertThat(compensationService).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Create employee returns a new employee")
+    public void create_givenAValidEmployeeId_ShouldPersistAndReturnCompensationDto() {
+
+        final String employeeId = "1";
+        Employee employee = new Employee(employeeId, "Ricardo", "Ianelli", "Software Engineer", "IT");
+
+        Compensation compensation = new Compensation(employeeId, new BigDecimal("10000"), LocalDate.parse("8/1/2022"));
+        CompensationDto dto = compensationAdapter.entityToDto(compensation);
+
+        when(employeeRepository.findByEmployeeId(employeeId)).thenReturn(employee);
+        when(compensationRepository.findByEmployeeId(employeeId)).thenReturn(null);
+        when(compensationRepository.insert(compensation)).thenReturn(compensation);
+
+        CompensationDto persisted = compensationService.create(dto);
+        CompensationComparer.compareDtos(persisted, dto);
+        verify(compensationRepository, times(1)).insert(compensation);
+    }
+
+    @Test(expected = EmployeeNotFoundException.class)
+    @DisplayName("Create compensation with invalid employee id throw exception")
+    public void create_givenInvalidEmployeeId_ShouldThrowException() {
+
+        final String employeeId = "1";
+        Compensation compensation = new Compensation(employeeId, new BigDecimal("10000"), LocalDate.parse("8/1/2022"));
+        CompensationDto dto = compensationAdapter.entityToDto(compensation);
+
+        when(compensationRepository.findByEmployeeId(employeeId)).thenReturn(null);
+        when(employeeRepository.findByEmployeeId(employeeId)).thenReturn(null);
+
+        compensationService.create(dto);
+
+        verify(employeeRepository, times(1)).findByEmployeeId(employeeId);
+        verify(compensationRepository, times(1)).findByEmployeeId(employeeId);
+        verify(compensationRepository, times(0)).insert(compensation);
+    }
+
+    @Test(expected = EmployeeNotFoundException.class)
+    @DisplayName("Create duplicate compensation should throw exception")
+    public void create_givenAlreadyExistingCompensation_ShouldThrowException() {
+
+        final String employeeId = "1";
+        Compensation compensation = new Compensation(employeeId, new BigDecimal("10000"), LocalDate.parse("8/1/2022"));
+        CompensationDto dto = compensationAdapter.entityToDto(compensation);
+
+        when(compensationRepository.findByEmployeeId(employeeId)).thenReturn(compensation);
+        when(employeeRepository.findByEmployeeId(employeeId)).thenReturn(null);
+
+        compensationService.create(dto);
+
+        verify(employeeRepository, times(1)).findByEmployeeId(employeeId);
+        verify(compensationRepository, times(1)).findByEmployeeId(employeeId);
+        verify(compensationRepository, times(0)).insert(compensation);
+    }
+
+    @Test
+    @DisplayName("Read compensation with a valid employee id returns a compensation from DB")
+    public void read_givenAValidEmployeeId_ShouldReturnCompensationDto() {
+
+        final String employeeId = "1";
+        Employee employee = new Employee(employeeId, "Ricardo", "Ianelli", "Software Engineer", "IT");
+
+        Compensation compensation = new Compensation(employeeId, new BigDecimal("10000"), LocalDate.parse("8/1/2022"));
+        CompensationDto dto = compensationAdapter.entityToDto(compensation);
+
+        when(compensationRepository.findByEmployeeId(employeeId)).thenReturn(compensation);
+        when(employeeRepository.findByEmployeeId(employeeId)).thenReturn(employee);
+
+        CompensationDto persistedCompensation = compensationService.read(employeeId);
+        CompensationComparer.compareDtos(persistedCompensation, dto);
+
+        verify(compensationRepository, times(1)).findByEmployeeId(employeeId);
+        verify(employeeRepository, times(1)).findByEmployeeId(employeeId);
+    }
+
+    @Test(expected = CompensationNotFoundException.class)
+    @DisplayName("Read compensation that doesn't exist throws CompensationNotFound exception")
+    public void read_givenANotPersistedCompensation_ShouldThrowCompensationNotFoundException() {
+        final String employeeId = "1";
+
+        when(compensationRepository.findByEmployeeId(employeeId)).thenReturn(null);
+
+        compensationService.read(employeeId);
+
+        verify(compensationRepository, times(1)).findByEmployeeId(employeeId);
+    }
+}
