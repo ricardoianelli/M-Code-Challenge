9cce43e20a44bb41323d8b8aff6621713ba9f748
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 16:08:27 -0500

    Implement ReportingStructure controller endpoint

--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -1,9 +1,13 @@
 package com.mindex.challenge.controller;
 
+import com.mindex.challenge.dto.ReportingStructure;
 import com.mindex.challenge.service.ReportingStructureService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,13 @@ public class ReportingStructureController {
 
     @Autowired
     private ReportingStructureService reportingStructureService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> generate(@PathVariable String id) {
+        LOG.debug("Received employee reporting structure request for id [{}]", id);
+        ReportingStructure report = reportingStructureService.generate(id);
+
+        LOG.debug("Responding report structure for employee id [{}]", id);
+        return ResponseEntity.ok(report);
+    }
 }
