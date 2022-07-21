e36df63d6724886d037d7d5cd7adb543aa5715f5
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 15:54:40 -0500

    Add basic ReportingStructure Controller

---
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -0,0 +1,17 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/report")
+public class ReportingStructureController {
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
+}
