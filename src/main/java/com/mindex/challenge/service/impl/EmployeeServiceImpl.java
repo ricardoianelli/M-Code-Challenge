e13f07843ca2531ac410631355c5cb017e917cf7
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-20 21:57:36 -0500

    Refactor EmployeeServiceImpl to pass tests

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.exceptions.EmployeeNotFoundException;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,31 +20,40 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee create(Employee employee) {
-        LOG.debug("Creating employee [{}]", employee);
+        LOG.debug("Creating employee id [{}]", employee.getEmployeeId());
 
         employee.setEmployeeId(UUID.randomUUID().toString());
         employeeRepository.insert(employee);
 
+        LOG.debug("Employee with id [{}] was created successfully", employee);
         return employee;
     }
 
     @Override
     public Employee read(String id) {
-        LOG.debug("Creating employee with id [{}]", id);
 
+        LOG.debug("Reading employee with id [{}]", id);
         Employee employee = employeeRepository.findByEmployeeId(id);
-
         if (employee == null) {
-            throw new RuntimeException("Invalid employeeId: " + id);
+            throw new EmployeeNotFoundException("Could not find employee with id " + id);
         }
 
+        LOG.debug("Returning employee with id [{}]", employee);
         return employee;
     }
 
     @Override
     public Employee update(Employee employee) {
-        LOG.debug("Updating employee [{}]", employee);
+        LOG.debug("Updating employee id [{}]", employee.getEmployeeId());
 
-        return employeeRepository.save(employee);
+        Employee existingEmployee = employeeRepository.findByEmployeeId(employee.getEmployeeId());
+
+        if (existingEmployee == null) {
+            LOG.debug("Employee not found during update. Inserting a new one with id [{}]", employee.getEmployeeId());
+        }
+
+        employeeRepository.save(employee);
+        LOG.debug("Employee with id [{}] updated or inserted successfully", employee.getEmployeeId());
+        return employee;
     }
 }
