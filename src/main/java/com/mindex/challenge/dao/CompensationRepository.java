6ef6e4d09af7d1d57967449672b9f9699d3f7a3e
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 16:56:25 -0500

    Create Compensation repository

---
+++ src/main/java/com/mindex/challenge/dao/CompensationRepository.java
@@ -0,0 +1,10 @@
+package com.mindex.challenge.dao;
+
+import com.mindex.challenge.data.Compensation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompensationRepository extends MongoRepository<Compensation, String> {
+    Compensation findByEmployeeId(String employeeId);
+}
