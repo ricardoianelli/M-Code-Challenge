193f5cbcb1c35c018afcdcc424736d4667b12978
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 15:35:03 -0500

    Create ReportingStructureService

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,42 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.adapter.EmployeeAdapter;
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.dto.EmployeeDto;
+import com.mindex.challenge.dto.ReportingStructureDto;
+import com.mindex.challenge.exceptions.EmployeeNotFoundException;
+import com.mindex.challenge.service.EmployeeService;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+    @Autowired
+    private EmployeeAdapter employeeAdapter;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public ReportingStructureDto read(String employeeId) {
+        LOG.debug("Getting employee with id [{}] from the database", employeeId);
+
+        Employee employee = employeeRepository.findByEmployeeId(employeeId);
+        if (employee == null) {
+            throw new EmployeeNotFoundException("Could not find employee with id " + employeeId);
+        }
+
+        EmployeeDto dto = employeeAdapter.entityToDto(employee);
+        ReportingStructureDto reportingStructureDto = new ReportingStructureDto(dto);
+
+        LOG.debug("Returning reporting structure for employee with id [{}]", employee);
+        return reportingStructureDto;
+    }
+}
