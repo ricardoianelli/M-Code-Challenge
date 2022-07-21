1a868774e2b42b6ff9c1026a4ef8f682821b875e
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-20 19:35:53 -0500

    Add create employee endpoint test

--- src/test/java/com/mindex/challenge/controller/EmployeeControllerTests.java
+++ src/test/java/com/mindex/challenge/controller/EmployeeControllerTests.java
@@ -1,5 +1,6 @@
 package com.mindex.challenge.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.exceptions.EmployeeNotFoundException;
 import com.mindex.challenge.service.EmployeeService;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,9 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(EmployeeController.class)
@@ -35,6 +36,14 @@ public class EmployeeControllerTests {
 
     final String BASE_ROUTE = "/employee";
 
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     @DisplayName("Smoke test for Spring DI")
     public void contextLoads() {
@@ -42,7 +51,7 @@ public class EmployeeControllerTests {
     }
 
     @Test
-    @DisplayName("Read endpoint with valid id returns employee and 200")
+    @DisplayName("Read employee endpoint with valid id returns employee and 200")
     public void read_givenAValidId_ShouldReturnEmployeeAnd200() throws Exception {
 
         final String employeeId = "123";
@@ -57,7 +66,7 @@ public class EmployeeControllerTests {
     }
 
     @Test
-    @DisplayName("Read endpoint with invalid id returns error and 404")
+    @DisplayName("Read employee endpoint with invalid id returns error and 404")
     public void read_givenANotExistentId_ShouldReturn404() throws Exception {
         when(employeeService.read(anyString())).thenThrow(EmployeeNotFoundException.class);
 
@@ -67,5 +76,26 @@ public class EmployeeControllerTests {
                 .andExpect(jsonPath("$.message").value("Employee not found"));
     }
 
+    @Test
+    @DisplayName("Create employee endpoint with valid employee format returns employee and 201")
+    public void create_givenAValidInput_ShouldReturnEmployeeAnd200() throws Exception {
 
+        final String employeeId = "123";
+        Employee exampleEmployee = new Employee(employeeId, "Ricardo", "Ianelli", "Software Engineer", "IT");
+
+        when(employeeService.read(employeeId)).thenReturn(exampleEmployee);
+
+        mockMvc.perform(post(BASE_ROUTE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(exampleEmployee)))
+
+                .andExpect(status().isCreated())
+                .andExpect(header().string(HttpHeaders.LOCATION, BASE_ROUTE + "/" + employeeId))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.employeeId").value(employeeId))
+                .andExpect(jsonPath("$.firstName").value(exampleEmployee.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(exampleEmployee.getLastName()))
+                .andExpect(jsonPath("$.position").value(exampleEmployee.getPosition()))
+                .andExpect(jsonPath("$.department").value(exampleEmployee.getDepartment()));
+    }
 }
