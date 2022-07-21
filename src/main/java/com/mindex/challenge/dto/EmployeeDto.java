f60cfa2670fe1d86dc1bf32a889cfcbbb71c27c6
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 10:13:09 -0500

    Add constructors to EmployeeDto

--- src/main/java/com/mindex/challenge/dto/EmployeeDto.java
+++ src/main/java/com/mindex/challenge/dto/EmployeeDto.java
@@ -1,5 +1,6 @@
 package com.mindex.challenge.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDto {
@@ -8,5 +9,21 @@ public class EmployeeDto {
     public String lastName;
     public String position;
     public String department;
-    public List<String> directReports;
+    public List<String> directReports = new ArrayList<>();
+
+    public EmployeeDto() {
+    }
+
+    public EmployeeDto(String employeeId, String firstName, String lastName, String position, String department) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.department = department;
+    }
+
+    public EmployeeDto(String employeeId, String firstName, String lastName, String position, String department, List<String> directReports) {
+        this(employeeId, firstName, lastName, position, department);
+        this.directReports = directReports;
+    }
 }
