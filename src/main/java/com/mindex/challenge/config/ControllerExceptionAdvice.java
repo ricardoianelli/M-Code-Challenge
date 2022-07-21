d753289dacb2d05230cff44baa095f1da5aed617
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-20 19:23:33 -0500

    Add ControllerExceptionAdvice

---
+++ src/main/java/com/mindex/challenge/config/ControllerExceptionAdvice.java
@@ -0,0 +1,28 @@
+package com.mindex.challenge.config;
+
+import com.mindex.challenge.controller.StandardError;
+import com.mindex.challenge.exceptions.EmployeeNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionAdvice {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<StandardError> handleEmployeeNotFound(HttpServletRequest request) {
+        String errorMsg = "Employee not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return getResponseError(errorMsg, status, request);
+    }
+
+    private ResponseEntity<StandardError> getResponseError(String message, HttpStatus status, HttpServletRequest request) {
+        StandardError error = new StandardError(Instant.now(), status.value(), message, request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+}
