88920c8e8ab99d8d778c4e15508dc8459be85364
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 11:51:09 -0500

    Refactor EmployeeController to use DTO

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,6 +1,6 @@
 package com.mindex.challenge.controller;
 
-import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.dto.EmployeeDto;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,12 +21,12 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody Employee employee) {
+    public ResponseEntity<?> create(@RequestBody EmployeeDto employee) {
         LOG.debug("Received employee create request for [{}]", employee);
         try {
-            Employee createdEmployee = employeeService.create(employee);
+            EmployeeDto createdEmployee = employeeService.create(employee);
             return ResponseEntity
-                    .created(new URI("/employee/" + createdEmployee.getEmployeeId()))
+                    .created(new URI("/employee/" + createdEmployee.employeeId))
                     .body(createdEmployee);
         } catch (URISyntaxException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -36,20 +36,21 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public ResponseEntity<?> read(@PathVariable String id) {
         LOG.debug("Received employee read request for id [{}]", id);
+        EmployeeDto existingEmployee = employeeService.read(id);
 
-        return ResponseEntity.ok(employeeService.read(id));
+        return ResponseEntity.ok(existingEmployee);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable String id, @RequestBody Employee employee) {
+    public ResponseEntity<?> update(@PathVariable String id, @RequestBody EmployeeDto employee) {
         LOG.debug("Received employee update request for id [{}] and employee [{}]", id, employee);
 
         try {
-            employee.setEmployeeId(id);
-            Employee updatedEmployee = employeeService.update(employee);
+            employee.employeeId = id;
+            EmployeeDto updatedEmployee = employeeService.update(employee);
             return ResponseEntity
                     .ok()
-                    .location(new URI("/employee/" + updatedEmployee.getEmployeeId()))
+                    .location(new URI("/employee/" + updatedEmployee.employeeId))
                     .body(updatedEmployee);
         } catch (URISyntaxException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
