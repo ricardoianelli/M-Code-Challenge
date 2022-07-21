a9596b8a871abe1c090afa5d950aee7265bad835
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-21 17:21:43 -0500

    Create CompensationAdapterImpl implementation

--- src/main/java/com/mindex/challenge/adapter/impl/CompensationAdapterImpl.java
+++ src/main/java/com/mindex/challenge/adapter/impl/CompensationAdapterImpl.java
@@ -1,25 +1,22 @@
 package com.mindex.challenge.adapter.impl;
 
 import com.mindex.challenge.adapter.CompensationAdapter;
-import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.dto.CompensationDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Component
 public class CompensationAdapterImpl implements CompensationAdapter {
-
-    @Autowired
-    CompensationRepository compensationRepository;
-
     @Override
     public Compensation dtoToEntity(CompensationDto dto) {
-        return null;
+        return new Compensation(dto.employeeId, new BigDecimal(dto.salary), LocalDate.parse(dto.effectiveDate));
     }
 
     @Override
     public CompensationDto entityToDto(Compensation compensation) {
-        return null;
+        return new CompensationDto(compensation.getEmployeeId(), compensation.getSalary().toString(), compensation.getEffectiveDate().toString());
     }
 }
