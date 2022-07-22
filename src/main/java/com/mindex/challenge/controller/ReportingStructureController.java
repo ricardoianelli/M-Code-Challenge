a1ec7fe8aa303a063c77089b01a72bfdb10a4f91
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-22 13:18:27 -0500

    Add API documentation to ReportingStructureController

--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -1,7 +1,13 @@
 package com.mindex.challenge.controller;
 
+import com.mindex.challenge.dto.EmployeeDto;
 import com.mindex.challenge.dto.ReportingStructure;
 import com.mindex.challenge.service.ReportingStructureService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name="Reporting Structure")
 @RequestMapping("/report")
 public class ReportingStructureController {
     private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
@@ -20,6 +27,12 @@ public class ReportingStructureController {
     private ReportingStructureService reportingStructureService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Generate employee reporting structure", responses = {
+            @ApiResponse(description = "Generated Successfully", responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReportingStructure.class))),
+            @ApiResponse(description = "Employee not found", responseCode = "404",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = StandardError.class)))
+    })
     public ResponseEntity<?> generate(@PathVariable String id) {
         LOG.debug("Received employee reporting structure request for id [{}]", id);
         ReportingStructure report = reportingStructureService.generate(id);
