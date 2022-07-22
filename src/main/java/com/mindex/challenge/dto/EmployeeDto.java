d2708a88a9b19a3baa6a34f153dc5476b094b7de
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-22 14:54:15 -0500

    Add EmployeeDto validation

--- src/main/java/com/mindex/challenge/dto/EmployeeDto.java
+++ src/main/java/com/mindex/challenge/dto/EmployeeDto.java
@@ -2,15 +2,25 @@ package com.mindex.challenge.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
 public class EmployeeDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public String employeeId;
+    @NotBlank
+    @Size(min = 2, message = "First name should have at least 2 characters")
     public String firstName;
+    @NotBlank
+    @Size(min = 2, message = "Last name should have at least 2 characters")
     public String lastName;
+    @NotBlank
+    @Size(min = 2, message = "Position name should have at least 2 characters")
     public String position;
+    @NotBlank
+    @Size(min = 2, message = "Department name should have at least 2 characters")
     public String department;
     public Set<String> directReports = new HashSet<>();
 
