3a0535afa9ea9292a1bdddd0bbec07e214e1e1ac
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-22 15:07:47 -0500

    Refactor CompensationDto

--- src/main/java/com/mindex/challenge/dto/CompensationDto.java
+++ src/main/java/com/mindex/challenge/dto/CompensationDto.java
@@ -1,13 +1,31 @@
 package com.mindex.challenge.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.PositiveOrZero;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class CompensationDto {
-    public String salary;
-    public String effectiveDate;
+    @PositiveOrZero
+    public BigDecimal salary;
+    @PastOrPresent
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    public LocalDate effectiveDate;
 
     public CompensationDto() {
     }
 
-    public CompensationDto(String salary, String effectiveDate) {
+    public CompensationDto(BigDecimal salary, LocalDate effectiveDate) {
         this.salary = salary;
         this.effectiveDate = effectiveDate;
     }
