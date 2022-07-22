cd1461173476cb87387eba6a92d805f99300d816
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-22 14:03:14 -0500

    Require transactions for repositories

--- src/main/java/com/mindex/challenge/dao/CompensationRepository.java
+++ src/main/java/com/mindex/challenge/dao/CompensationRepository.java
@@ -3,10 +3,13 @@ package com.mindex.challenge.dao;
 import com.mindex.challenge.data.Compensation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface CompensationRepository extends MongoRepository<Compensation, String> {
     Compensation findByEmployeeId(String employeeId);
     List<Compensation> findAllByEmployeeId(String employeeId);
--- src/main/java/com/mindex/challenge/dao/EmployeeRepository.java
+++ src/main/java/com/mindex/challenge/dao/EmployeeRepository.java
@@ -3,8 +3,11 @@ package com.mindex.challenge.dao;
 import com.mindex.challenge.data.Employee;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(propagation = Propagation.MANDATORY)
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
     Employee findByEmployeeId(String employeeId);
 }
