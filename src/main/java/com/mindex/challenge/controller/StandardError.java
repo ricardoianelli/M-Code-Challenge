b8a82188c42853152b5bf1e32b435353c463c630
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-22 13:13:50 -0500

    Rename status property on StandardError

--- src/main/java/com/mindex/challenge/controller/StandardError.java
+++ src/main/java/com/mindex/challenge/controller/StandardError.java
@@ -9,7 +9,7 @@ import java.time.Instant;
 public class StandardError implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
-    private Integer status;
+    private Integer statusCode;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String message;
     private String path;
@@ -17,9 +17,9 @@ public class StandardError implements Serializable {
     public StandardError() {
     }
 
-    public StandardError(Instant timestamp, Integer status, String message, String path) {
+    public StandardError(Instant timestamp, Integer statusCode, String message, String path) {
         this.timestamp = timestamp;
-        this.status = status;
+        this.statusCode = statusCode;
         this.message = message;
         this.path = path;
 
@@ -36,12 +36,12 @@ public class StandardError implements Serializable {
         this.timestamp = timestamp;
     }
 
-    public Integer getStatus() {
-        return status;
+    public Integer getStatusCode() {
+        return statusCode;
     }
 
-    public void setStatus(Integer status) {
-        this.status = status;
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
     }
 
     public String getMessage() {
