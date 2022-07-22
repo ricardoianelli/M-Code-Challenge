57082cc6604dc65d1520f905f3a7bab603829799
Ricardo Ianelli <ricardoianelli@hotmail.com>
2022-07-22 13:12:46 -0500

    Hide id from employee creation request

--- src/main/java/com/mindex/challenge/dto/EmployeeDto.java
+++ src/main/java/com/mindex/challenge/dto/EmployeeDto.java
@@ -1,9 +1,12 @@
 package com.mindex.challenge.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class EmployeeDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public String employeeId;
     public String firstName;
     public String lastName;
